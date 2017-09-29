7c6e193d5354a6d9a9e1bfe6d45081bd3463ac25
William Casey <williamclarkecasey@gmail.com>
2017-09-29 14:52:01 -0500

    updated file

--- sign_out_request.php
+++ sign_out_request.php
@@ -2,6 +2,8 @@
 
 $post_data = array("student" => $_POST["student"], "location" => $_POST["location"], "companion" => $_POST["companion"], "return_time" => $_POST["return_time"]);
 
+var_dump($post_data);
+
 
 
 
