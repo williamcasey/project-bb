0973dd24e1aae7c979e31efbdb31a856d937d66d
William Casey <williamclarkecasey@gmail.com>
2017-09-29 20:41:19 -0500

    lsmsa.php

---
+++ lsmsa.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+
+        <title>LSMSA Sign Out</title>
+        <meta name="apple-mobile-web-app-title" content="LSMSA Sign Out">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3mobile.css">
+        <meta name="apple-mobile-web-app-status-bar-style" content="white">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <link rel="apple-touch-icon" href="./lsmsa-logo.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="./lsmsa-logo.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="./lsmsa-logo.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="./lsmsa-logo.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="./lsmsa-logo.png">
+        <link rel="apple-touch-icon" sizes="192x192" href="./lsmsa-logo.png">
+        <link rel="apple-touch-startup-image" href="./lsmsa-logo-big.png">
+        <style>
+            body {
+                font-family: "Helvetica", sans-serif;
+            }
+        </style>
+    </head>
+
+    <body>
+        <h3>THE Louisiana School for Math, Science and the Arts</h3>
+
+    </body>
+
+</html>
